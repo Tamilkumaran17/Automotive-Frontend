661b4e747aa9e532ff4f0297ca0006f800c447e0
Tamilkumaran <123346938+Tamilkumaran17@users.noreply.github.com>
2024-08-29 09:24:23 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: ['es2022', 'chrome89', 'edge89', 'firefox79', 'safari14.1'],
+  },
 })
